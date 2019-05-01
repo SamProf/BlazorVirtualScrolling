a057c0a40aa5f633ab242a91ed62dcaea733f317
SamProf <wsamprof@gmail.com>
2019-05-01 23:12:29 +0200

    first api of the component and first use example

---
+++ src/BlazorVirtualScrolling/Components/VirtualScroll/BaseVirtualScroll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorVirtualScrolling
+{
+    public class BaseVirtualScroll<ItemType> : ComponentBase
+    {
+        [Parameter]
+        public string Class { get; set; }
+
+        [Parameter]
+        public string Style { get; set; }
+
+        [Parameter]
+        public int RowHeight { get; set; } = 50;
+
+        public ElementRef Ref { get; set; }
+
+        [Parameter]
+        public RenderFragment<ItemType> ChildContent { get; set; }
+
+        [Parameter]
+        public IEnumerable<ItemType> Items { get; set; }
+    }
+}
